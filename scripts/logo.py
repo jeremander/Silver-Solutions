efc943b5a1a0d68d717eb530f990341d17de2725
Jeremy Silver <jeremyag@comcast.net>
2020-09-05 10:36:37 -0400

    About to revise logo generation.

--- scripts/logo.py
+++ scripts/logo.py
@@ -7,9 +7,6 @@ import drawSvg as draw
 
 DEG2RAD = 2 * np.pi / 360
 
-alpha = np.arcsin(np.sqrt(3)/3)
-beta = np.pi / 4
-
 camera_pos = np.array([0, 0, -40])
 surface_pos = np.array([0, 0, 40])
 theta = np.array([0, 0, 0])
@@ -29,7 +26,7 @@ B = np.array([[np.cos(theta[1]), 0, -np.sin(theta[1])], [0, 1, 0], [np.sin(theta
 C = np.array([[np.cos(theta[2]), np.sin(theta[2]), 0], [-np.sin(theta[2]), np.cos(theta[2]), 0], [0, 0, 1]])
 D = np.array([[1, 0, surface_pos[0] / surface_pos[2]], [0, 1, surface_pos[1] / surface_pos[2]], [0, 0, 1 / surface_pos[2]]])
 
-phi = DEG2RAD * np.array([22, 45, 0])
+phi = DEG2RAD * np.array([24, 45, 0])
 rx = Rx(phi[0])
 ry = Ry(phi[1])
 rz = Rz(phi[2])
@@ -56,7 +53,7 @@ f_points = list(filter(f_face, cube_grid))
 r_points = list(filter(r_face, cube_grid))
 
 def square(pts, inds):
-    return [pts[i] for i in inds]
+    return tuple(pts[i] for i in inds)
 
 ufl = square(u_points, [0, 1, 5, 4])
 uf = square(u_points, [4, 5, 9, 8])
@@ -78,41 +75,81 @@ ful = square(f_points, [2, 3, 7, 6])
 fu = square(f_points, [6, 7, 11, 10])
 fur = square(f_points, [10, 11, 15, 14])
 
-rdf = square(f_points, [0, 1, 5, 4])
-rf = square(f_points, [4, 5, 9, 8])
-ruf = square(f_points, [8, 9, 13, 12])
-rd = square(f_points, [1, 2, 6, 5])
-rc = square(f_points, [5, 6, 10, 9])
-ru = square(f_points, [9, 10, 14, 13])
-rdb = square(f_points, [2, 3, 7, 6])
-rb = square(f_points, [6, 7, 11, 10])
-rub = square(f_points, [10, 11, 15, 14])
+rdf = square(r_points, [0, 1, 5, 4])
+rf = square(r_points, [4, 5, 9, 8])
+ruf = square(r_points, [8, 9, 13, 12])
+rd = square(r_points, [1, 2, 6, 5])
+rc = square(r_points, [5, 6, 10, 9])
+ru = square(r_points, [9, 10, 14, 13])
+rdb = square(r_points, [2, 3, 7, 6])
+rb = square(r_points, [6, 7, 11, 10])
+rub = square(r_points, [10, 11, 15, 14])
+
+squares = [ufl, uf, ufr, ul, uc, ur, ubl, ub, ubr, fdl, fd, fdr, fl, fc, fr, ful, fu, fur, rdf, rf, ruf, rd, rc, ru, rdb, rb, rub]
+
+silver = '#c0c0cf'
+black = '#000000'
+white = '#ffffff'
+
+square_colors = {sq : black for sq in squares}
+square_colors.update({sq : silver for sq in [fdl, fd, fc, fu, fur, rdf, rd, rc, ru, rub]})
+
+def line_is_visible(pt1, pt2):
+    good = False
+    good |= u_face(pt1) and u_face(pt2)
+    good |= f_face(pt1) and f_face(pt2)
+    good |= r_face(pt1) and r_face(pt2)
+    good |= (u_face(pt1) and f_face(pt2)) or (f_face(pt1) and u_face(pt2))
+    good |= (u_face(pt1) and r_face(pt2)) or (r_face(pt1) and u_face(pt2))
+    return good
+
+def extend_line(pt1, pt2, c = 1.0):
+    pt1 = np.array(pt1)
+    pt2 = np.array(pt2)
+    v = pt2 - pt1
+    return (pt1 + ((c + 1) / 2) * v, pt1 - (c - 1) * v / 2)
+
+c = 1.0
+
+lines = [extend_line(*pair, c = c) for pair in itertools.combinations(cube_grid, 2) if (sum((i1 == i2) for (i1, i2) in zip(*pair)) == 2) and line_is_visible(*pair)]
+
+def is_top_point(pt):
+    return (pt[1] == 3) and (pt[2] != -3) and (pt[0] != 3)
+
+line_width = 0.22
+top_line_width_scale = 0.75
+
+# def is_outer_
 
 
 
 if __name__ == '__main__':
 
-    arr = [-3, -1, 1, 3]
-    grid = list(itertools.product(arr, arr, arr))
-    edges = []
-    for pair in itertools.combinations(grid, 2):
-        if sum((i1 == i2) for (i1, i2) in zip(*pair)) == 2:
-            edges.append(pair)
 
-    d = draw.Drawing(15, 15, origin = 'center')
-    for (pt1, pt2) in edges:
-        good = False
-        good |= u_face(pt1) and u_face(pt2)
-        good |= f_face(pt1) and f_face(pt2)
-        good |= r_face(pt1) and r_face(pt2)
-        good |= (u_face(pt1) and f_face(pt2)) or (f_face(pt1) and u_face(pt2))
-        good |= (u_face(pt1) and r_face(pt2)) or (r_face(pt1) and u_face(pt2))
-        if good:
-            p1 = project(pt1)
-            p2 = project(pt2)
-            # p1 = tuple(np.dot(proj_iso, np.array(pt1)))[:2]
-            # p2 = tuple(np.dot(proj_iso, np.array(pt2)))[:2]
-            d.append(draw.Line(*(p1 + p2), stroke = 'black', stroke_width = 0.1))
+    # arr = [-3, -1, 1, 3]
+    # grid = list(itertools.product(arr, arr, arr))
+    # edges = []
+    # for pair in itertools.combinations(grid, 2):
+    #     if sum((i1 == i2) for (i1, i2) in zip(*pair)) == 2:
+    #         edges.append(pair)
+
+    d = draw.Drawing(10, 10, origin = 'center')
+
+    yshift = np.array([0, 0.3])
+
+    d.append(draw.Rectangle(-100, -100, 200, 200, stroke_width = 0, fill = '#ff0000'))
+
+    for (sq, color) in square_colors.items():
+        points = [project(pt) + yshift for pt in sq]
+        d.append(draw.Lines(*(x for pt in points for x in pt), close = True, fill = color, stroke_width = 0))
+
+    for line in lines:
+        stroke_width = line_width
+        if is_top_point(line[0]) or is_top_point(line[1]):
+            stroke_width *= top_line_width_scale
+        points = [project(pt) + yshift for pt in line]
+        d.append(draw.Line(*(x for pt in points for x in pt), stroke = white, stroke_width = stroke_width))
+
 
     d.setPixelScale(100)
     d.saveSvg('logo.svg')
